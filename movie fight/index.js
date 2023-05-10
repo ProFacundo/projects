8ca7269fcf534b1dde1436a11f7dca54a8558341
ProFacundo <102171685+ProFacundo@users.noreply.github.com>
2023-05-11 00:36:47 +0100

    a few changes

--- movie fight/index.js
+++ movie fight/index.js
@@ -9,19 +9,41 @@ const fetchData = async (searchTerm)=>{
     return response.data.Search
 }
 
-
+const root=document.querySelector('.autocomplete')
+root.innerHTML=`
+<label><b> Search for movie</label>
+<input class="input"/>
+<div class="dropdown">
+<div class="dropdown menu">
+<div class="dropdown-content results"></div>
+</div>
+</div>
+`
+const input= document.querySelector('input')
+const dropDown= document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async (e)=>{
     const movies = await fetchData(e.target.value)
-    for(let movie of movies){
-        const div=document.createElement('div')
-         
-        div.innerHTML=`
-        <img src="${movie.Poster}"/>
-        <h1>${movie.Title}</h1>`
 
-        document.querySelector('#target').appendChild(div)
+    resultsWrapper.innerHTML=""
+    dropDown.classList.add('is-active')
+    for(let movie of movies){
+        const option=document.createElement('a')
+        const imgSRC = movie.Poster==='N/A'? '': movie.Poster 
+
+        option.classList.add('dropdown-item')
+        option.innerHTML=`
+        <img src="${imgSRC}"/>
+        ${movie.Title}`
+
+        resultsWrapper.appendChild(option)
     }
 }
-const input= document.querySelector('input')
+
 input.addEventListener('input',debounce(onInput,500))
+
+document.addEventListener('click',e=>{
+    if(!root.contains(e.target)){
+        dropDown.classList.remove('is-active')}
+    })
